02b7b9f827e6cbbda706f2fa0640c5ede533d786
YOUR NAME <YOUR@EMAIL.com>
2015-07-07 11:31:16 +0700

    haon chinh

--- hethongthitracnghiem/db/migrate/20150629112744_create_lops.rb
+++ hethongthitracnghiem/db/migrate/20150629112744_create_lops.rb
@@ -1,7 +1,7 @@
 class CreateLops < ActiveRecord::Migration
   def change
     create_table :lops do |t|
-      t.string :ten,              null: false, default: ""
+      t.string :tenlop,              null: false, default: ""
       t.integer :khoa_id,              null: false, default: 0
     end
   end
